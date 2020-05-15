14f78a25e65ed9789a8e06256f81567d59bc01da
Daniel Mamphekgo <Danielromeo99@gmail.com>
2020-05-15 22:11:56 +0200

    failed to Make the Select in the modal, show preExisting selection when updating a user

--- classes/views/usersview.class.php
+++ classes/views/usersview.class.php
@@ -12,6 +12,8 @@ class UsersView extends Users{
 		echo "Full name: ".$results[0]['firstname']. ' '.$results[0]['lastname'];
 	}
 
+
+	// this function shows all users along with a modal attached at the end for editing the records
 	public function showAllUsers(){
 		$results = $this->getUsers();
 		//echo json_encode($results);
@@ -37,13 +39,65 @@ class UsersView extends Users{
 					<td>".$row['firstname']."</td>
 					<td>".$row['lastname']."</td>
 					<td> 
-						<a class='btn btn-warning'>Edit</a>
+						<button data-target='#editModal".$row['id']."' data-toggle='modal' type='button' class='btn btn-warning'>Edit</button>
 					</td>
 
 					<td> 
 						<a class='btn btn-danger'>Delete</a>
 					</td>			
-				</tr>"
+				</tr>
+
+				<div class='modal fade' id='editModal".$row['id']."' tabindex='-1' role='dialog' aria-labelledby='editModal' aria-hidden='true'>
+						<div class='modal-dialog modal-dialog-centered' role='document'>
+						<div class='modal-content'>
+						  <div class='modal-header'>
+						    <h5 class='modal-title' id='editModal'>Edit User</h5>
+						    <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+						      <span aria-hidden='true'>&times;</span>
+						    </button>
+						  </div>
+						  <div class='modal-body'>
+
+						  		<div class='container'>
+							  		<div id='row'>
+
+										<form action='index.php' method='POST'>
+
+											<div class='form-group'>
+												<label for=''>First name:</label>
+												<input value='".$row['firstname']."' name='firstname' type='text' class='form-control' id='' required>
+											</div>
+
+											<div class='form-group'>
+												<label for=''>Last Name:</label>
+												<input value='".$row['lastname']."' name='lastname' type='text' class='form-control' required>
+											</div>
+
+											<div class='form-group'>
+												<label for=''>Date Of Birth:</label>
+												<input value='".$row['dob']."' name='dob' type='text' class='form-control' placeholder='eg. 2020-04-20' required>
+											</div>
+
+											<div class='form-group'>
+												<label for=''>Home Language:</label>
+												<select name='language' class='form-control' required>
+													<option value='English'>English</option>
+													<option value='Sepedi'>Sepedi</option>
+													<option value='Zulu'>isiZulu</option>
+													<option value='Afrikaans'>Afrikaans</option>
+												</select>
+											</div>
+
+											<button id='submitButton' type='submit' name='submit' class='btn btn-primary'>Submit</button>
+										</form>
+									</div>
+								</div>	
+						  	</div>
+
+						  	
+						</div>
+						</div>
+					</div>"
 			;
 		}
 		
